7004b2c4e921f2006f3f4e73d649f9bd2773d023
Sacha Sedriks <sachased@gmail.com>
2020-03-25 15:43:23 -0700

    completed DOM traversing, event listener jQuery exercise.

---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+// $(document).ready
+$(function() {
+  createItem();
+  toggleCheck();
+  deleteItem();
+});
+
+
+function createItem() {
+  $('#js-shopping-list-form').submit(event => {
+    // console.log(event.type, event.currentTarget); // test
+
+    // to prevent the default form submission behaviour
+    event.preventDefault();
+    // capture what was submitted
+    const userItem = $(event.currentTarget).find('#shopping-list-entry');
+    // generate an html list item
+    // instead of .append'ing item at the end of the <ul> element
+    // gonna .preprend item to the beginning
+    // as I prefer the UX ;P
+    $('ul').prepend(`<li><span class="shopping-item">${userItem.val()}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
+    // then, clear input
+    userItem.val('');
+  });
+}
+
+function toggleCheck() {
+  $('ul.shopping-list').on('click', 'button.shopping-item-toggle', event => {
+    // !! fuck yeah, traversing the tree !! =D 
+    const targetItem = $(event.currentTarget).closest('li').children('span'); 
+    targetItem.toggleClass('shopping-item__checked');
+  });
+}
+
+function deleteItem() {
+  $('ul.shopping-list').on('click', 'button.shopping-item-delete', event => {
+    const targetItem = $(event.currentTarget).closest('li'); 
+    targetItem.remove();
+  });
+}
+
+
+// PSEUDOCODE INSTRUCTIONS /////////////////////////////////////////
+// per: https://courses.thinkful.com/interactive-web-apps-v1/checkpoint/3#shopping-list-challenge
+
+// 1
+// Create <li> item 
+// by entering text into <form> <input>
+// and hitting 'Return' .keydown
+// or .click'ing on 'Add item' <button>
+
+// 2
+// within <li> items
+// check and uncheck (toggle) items text (strikethru style)
+// by .click'ing the 'Check' <button>
+
+// 3
+// permanently remove <li> item by .clicking'ing 'Delete' button
+
+// HINT
+// consider these jQuery methods 
+// .submit()
+// .preventDefault()
+// .toggleClass()
+// .closest()
